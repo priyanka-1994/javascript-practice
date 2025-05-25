87ac565da9fb8a3473dbe0a2f00a514a938351d0
priyanka-1994 <71969309+priyanka-1994@users.noreply.github.com>
2025-05-25 11:32:57 +0530

    Create script.js

---
+++ Day2/script.js
@@ -0,0 +1,40 @@
+// selecting elements
+const input = document.getElementById('taskInput')
+const addBtn = document.getElementById('addbtn')
+const taskList = document.getElementById('taskList')
+
+// Add Task on button list
+addBtn.addEventListener('click',() => {
+   const task = input.value.trim();
+   if(task === '') return;
+   
+   // create list item
+   const li = document.createElement('li');
+   li.textContent = task;
+   li.setAttribute('data-status','pending');
+   li.style.cursor='pointer';
+   
+   // toggle done/undone
+   li.addEventListener('click',() => {
+      const status = li.getAttribute('data-status');
+      if(status === 'pending'){
+         li.classList.add('done');
+         li.setAttribute('data-status','data');
+      } else {
+        li.classList.remove('done');
+        li.setAttribute('data-status','pending')
+      }   
+   });
+   const deleteBtn = document.createElement('button');
+   deleteBtn.innerText = 'X';
+   deleteBtn.style.marginLeft = '10px';
+   deleteBtn.setAttribute('title','Delete Task');
+   deleteBtn.addEventListener('click',(e) => {
+      e.stopPropagation();
+      const parentLi = deleteBtn.parentElement;
+      taskList.removeChild(parentLi);
+   });
+   li.append(deleteBtn);
+   taskList.appendChild(li);
+   input.value = '';
+});
