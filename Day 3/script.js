3115332a7077527bac0e8d0132370c114ab67cf2
priyanka-1994 <71969309+priyanka-1994@users.noreply.github.com>
2025-05-26 06:26:58 +0530

    Create script.js

---
+++ Day 3/script.js
@@ -0,0 +1,13 @@
+let countEl = document.getElementById("count-el")
+console.log(countEl)
+
+let count = 0
+
+function increment() {
+  count += 1
+  countEl.innerText = count
+}
+
+function save() {
+  console.log(count)
+}
