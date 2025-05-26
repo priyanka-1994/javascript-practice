c43b1b94472bb3dff6b8d603e11b707ababcf692
priyanka-1994 <71969309+priyanka-1994@users.noreply.github.com>
2025-05-26 08:07:04 +0530

    Update script.js

--- Day 3/script.js
+++ Day 3/script.js
@@ -1,13 +1,18 @@
+let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
-console.log(countEl)
-
 let count = 0
+
+console.log(saveEl)
 
 function increment() {
   count += 1
-  countEl.innerText = count
+  countEl.textContent = count
 }
 
 function save() {
-  console.log(count)
+  let countStr = count + " - "
+  saveEl.textContent += countStr
+  countEl.textContent = 0
+  count = 0
 }
+
