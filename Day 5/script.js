43f2efe2219b3f7d6b709ad3a4782e519f25108f
priyanka-1994 <71969309+priyanka-1994@users.noreply.github.com>
2025-05-27 17:27:38 +0530

    Create script.js

---
+++ Day 5/script.js
@@ -0,0 +1,42 @@
+// chrome://extensions/
+let myLeads = []
+
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+if(leadsFromLocalStorage){
+  myLeads = leadsFromLocalStorage
+  renderLeads()
+}
+deleteBtn.addEventListener("dblclick",function(){
+  console.log("double clicked!")
+  localStorage.clear()
+  myLeads = []
+  renderLeads()
+})
+
+inputBtn.addEventListener("click", function(){
+  myLeads.push(inputEl.value)
+  inputEl.value = ''
+  // PS: remember JSON.stringify()
+  localStorage.setItem("myLeads",JSON.stringify(myLeads))
+  renderLeads()
+  console.log(localStorage.getItem("myLeads"))
+})
+
+function renderLeads(){
+  let listItems = ""
+  for(let i = 0; i < myLeads.length; i++){
+    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+    // listItems += "<li><a target = '_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a>" +     "</li>"
+    listItems += `
+          <li>
+             <a target= '_blank' href = "${myLeads[i]}"> ${myLeads[i]} 
+             </a>
+          </li>`
+  }
+  ulEl.innerHTML = listItems
+}
