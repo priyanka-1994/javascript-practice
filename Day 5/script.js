a717fd6f98d9e74e17603fa9c6d4bb5773003b74
priyanka-kale <priyanka-1994@gmail.com>
2025-05-28 08:34:38 +0530

    updating folder for day 5

--- Day 5/script.js
+++ Day 5/script.js
@@ -1,21 +1,47 @@
 // chrome://extensions/
 let myLeads = []
-
+let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn = document.getElementById("tab-btn")
+
 if(leadsFromLocalStorage){
   myLeads = leadsFromLocalStorage
-  renderLeads()
+  render(myLeads)
 }
+const tabs = [
+  {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+tabBtn.addEventListener("click",function(){
+  chrome.tabs.query({active: true, currentWindow: true},function(tabs){
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    render(myLeads)
+  });
+})
+function render(leads){
+  let listItems = ""
+  for(let i = 0; i < leads.length; i++){
+    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+    // listItems += "<li><a target = '_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a>" +     "</li>"
+    listItems += `
+          <li>
+             <a target= '_blank' href = "${leads[i]}"> ${leads[i]} 
+             </a>
+          </li>`
+  }
+  ulEl.innerHTML = listItems
+}
+
 deleteBtn.addEventListener("dblclick",function(){
   console.log("double clicked!")
   localStorage.clear()
   myLeads = []
-  renderLeads()
+  render(myLeads)
 })
 
 inputBtn.addEventListener("click", function(){
@@ -23,20 +49,6 @@ inputBtn.addEventListener("click", function(){
   inputEl.value = ''
   // PS: remember JSON.stringify()
   localStorage.setItem("myLeads",JSON.stringify(myLeads))
-  renderLeads()
+  render(myLeads)
   console.log(localStorage.getItem("myLeads"))
 })
-
-function renderLeads(){
-  let listItems = ""
-  for(let i = 0; i < myLeads.length; i++){
-    // ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-    // listItems += "<li><a target = '_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a>" +     "</li>"
-    listItems += `
-          <li>
-             <a target= '_blank' href = "${myLeads[i]}"> ${myLeads[i]} 
-             </a>
-          </li>`
-  }
-  ulEl.innerHTML = listItems
-}
