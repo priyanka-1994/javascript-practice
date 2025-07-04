b95ecde6013cc49594c7ee89ccf844cc18b066b7
priyanka-1994 <71969309+priyanka-1994@users.noreply.github.com>
2025-07-04 16:16:39 +0530

    Update script.js

--- Day 23 - Random-Password-Generator/script.js
+++ Day 23 - Random-Password-Generator/script.js
@@ -1,32 +1,51 @@
-const generateButton = document.getElementById('generate-pwd');
-const Password = document.getElementById('pwd');
-const uppercaseSense = document.getElementById('uppercase');
-const passwordLengthSlider = document.getElementById("passwordLength");
-const lengthValueDisplay = document.getElementById('lengthValue');
+const passwordInput = document.getElementById("password");
+const passwordLength = document.getElementById ('length');
+const upperCase = document.getElementById('uppercase');
+const lowerCase = document.getElementById('lowercase');
+const Numbers = document.getElementById('numbers');
+const Symbols = document.getElementById('symbols');
 
-const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let password = "";
+let charTouse = "";
 
-passwordLengthSlider.addEventListener("input",() => {
-    lengthValueDisplay.textContent = passwordLengthSlider.value;
-});
+const uppercaseChars  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercaseChars  = "abcdefghijklmnopqrstuvwxyz";
+const numberChars  = "1234567890";
+const symbolChars = "!@#$%^&*()_+-=[]{};':\"|,.<>/?";
 
-generateButton.addEventListener("click", () => {
-        let generatedPassword = "";
-        let charToUse = "";
-        if(uppercaseSense.checked) {
-            charToUse += uppercaseChars;
-        }
-        if(charToUse.length === 0){
-            Password.value="Select Option!";
-            return;
-        }
-        const passwordLength = parseInt(passwordLengthSlider.value);
-
-        for(let i=0; i<=passwordLength; i++) {
-            const randomIndex = Math.floor(Math.random()*charToUse.length);
-            generatedPassword += charToUse.charAt(randomIndex);
-        } 
-        Password.value = generatedPassword;
+function generatePassword() {
+    password = "";
+    charTouse = "";
     
+    const includepasswordLength = parseInt(passwordLength.value);
+    const includeupperCase = upperCase.checked;
+    const includelowerCase = lowerCase.checked;
+    const includeNumbers = Numbers.checked;
+    const includeSymbols = Symbols.checked;
+
+    if(includeupperCase) charTouse+=uppercaseChars ;
+    if(includelowerCase) charTouse+=lowercaseChars ;
+    if(includeNumbers) charTouse+=numberChars ;
+    if(includeSymbols) charTouse+=symbolChars;
+
+    if(charTouse.length === 0) {
+        passwordInput.value = "Select at least one optional!";
+        return;
+    } 
+
+    for(let i=0; i<includepasswordLength;i++){
+        const randomIndex = Math.floor(Math.random() * charTouse.length);
+        password += charTouse.charAt(randomIndex);
+    }
+    passwordInput.value = password;
+ }
+ function copyPassword(){
+    const passwordd = passwordInput.value;
+    navigator.clipboard.writeText(passwordd)
+    .then(() => {
+        alert("Password copied to clipboard")
+    })
+    .catch(err=> {
+        console.error("Failed to copy password: ", err);
     });
-lengthValueDisplay.textContent = passwordLengthSlider.value;
+ }
