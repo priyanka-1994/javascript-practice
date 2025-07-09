9a65f5ae453d6ebde79008f8c169e9e5fc3f3f80
priyanka-1994 <71969309+priyanka-1994@users.noreply.github.com>
2025-07-09 09:47:25 +0530

    Update script.js

--- Day 24 - Expense Tracker/script.js
+++ Day 24 - Expense Tracker/script.js
@@ -8,19 +8,37 @@ function addExpense() {
 
     if(Title && !isNaN(Price)){
         const time = new Date().toLocaleString();
-        expenses.push({Title,Price,time});
+        const newexpenses = { Title,Price,time };
+        expenses.push(newexpenses);
         
         const li = document.createElement('li');
-        li.textContent = `${Title} - ₹${Price} (Added on ${time})`;        
+        li.textContent = `${Title} - ₹${Price} (Added on ${time})`;   
+        
+        const btn = document.createElement('button');
+        btn.textContent = "❌";
+        btn.style.marginLeft = "10px";
+        btn.onclick = () => {
+            const index = expenses.indexOf(newexpenses);
+            if(index !== -1) {
+                expenses.splice(index,1);
+                li.remove();
+
+                const total = expenses.reduce((acc, item) => acc + item.Price, 0);
+                Expense.textContent = `Total: ₹${total}`;
+            }
+        };
+        li.appendChild(btn);
         listExpense.appendChild(li);
         const total = expenses.reduce((acc, item) => acc + item.Price, 0);
         Expense.textContent = `Total: ₹${total}`;
+    
+        document.getElementById('title').value = "";
+        document.getElementById('price').value = "";
     }
-    document.getElementById('title').value = "";
-    document.getElementById('price').value = "";
 }
 function clearExpenses(){
     expenses.length = 0;
     listExpense.innerHTML = "";
-    Expense.textContent = "Total: ₹0";
+    Expense.textContent = "All expenses cleared. Total: ₹0";
 }
+
