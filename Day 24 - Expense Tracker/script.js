831678e6882d2456b3edf744f96c18cde15c4ac0
priyanka-1994 <71969309+priyanka-1994@users.noreply.github.com>
2025-07-09 09:04:28 +0530

    Update script.js

--- Day 24 - Expense Tracker/script.js
+++ Day 24 - Expense Tracker/script.js
@@ -7,15 +7,20 @@ function addExpense() {
     const Price = parseFloat(document.getElementById('price').value);
 
     if(Title && !isNaN(Price)){
-        expenses.push({Title,Price});
+        const time = new Date().toLocaleString();
+        expenses.push({Title,Price,time});
         
         const li = document.createElement('li');
-        li.textContent = `${Title} - ₹${Price}`;
+        li.textContent = `${Title} - ₹${Price} (Added on ${time})`;        
         listExpense.appendChild(li);
         const total = expenses.reduce((acc, item) => acc + item.Price, 0);
         Expense.textContent = `Total: ₹${total}`;
     }
     document.getElementById('title').value = "";
     document.getElementById('price').value = "";
-
+}
+function clearExpenses(){
+    expenses.length = 0;
+    listExpense.innerHTML = "";
+    Expense.textContent = "Total: ₹0";
 }
