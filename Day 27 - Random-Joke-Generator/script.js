e2bc5cdb2eed0a7b78214b43bc7bc0c8e962ba5c
priyanka-1994 <71969309+priyanka-1994@users.noreply.github.com>
2025-07-27 08:10:18 +0530

    Update script.js

--- Day 27 - Random-Joke-Generator/script.js
+++ Day 27 - Random-Joke-Generator/script.js
@@ -11,6 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(res=> res.json())
         .then(data => {
             jokeText.innerText = data.joke; 
+            saveJokes(data.joke);
         })
         .catch(error => {
             jokeText.innerText = "Oops! Could not fetch a joke";
@@ -18,3 +19,14 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+function saveJokes(joke) {
+    const jokeHistory = JSON.parse(localStorage.getItem("jokes")) || [];
+    jokeHistory.push(joke);
+    localStorage.setItem('jokes',JSON.stringify(jokeHistory));
+}
+
+function loadJokes() {
+    let jokeHistory = JSON.parse(localStorage.getItem('jokes')) || [];
+    return jokeHistory; 
+}
